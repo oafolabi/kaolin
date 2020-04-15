797316ffbb7d0eccbb2f853e279d6c87d169aff5
akashgokul <akashgokul@berkeley.edu>
2020-04-14 23:57:10 -0700

    fixing package import

--- examples/Classification/pointcloud_classification.py
+++ examples/Classification/pointcloud_classification.py
@@ -5,7 +5,7 @@ import torch
 from torch.utils.data import DataLoader
 
 import sys
-sys.path.append("~//kaolin/")
+sys.path.append("../..")
 import kaolin
 from kaolin.datasets import ModelNet, Scan2CAD
 from kaolin.models.PointNet import PointNetClassifier
