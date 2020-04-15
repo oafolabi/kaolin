46203d192438b670aaee433d499fe7ec44668690
akashgokul <akashgokul@berkeley.edu>
2020-04-15 00:08:10 -0700

    trying to fix import

--- examples/Classification/pointcloud_classification.py
+++ examples/Classification/pointcloud_classification.py
@@ -5,8 +5,7 @@ import torch
 from torch.utils.data import DataLoader
 
 import sys
-sys.path.append("../..")
-from kaolin.datasets import ModelNet, Scan2CAD
+from scan2cad import Scan2CAD
 from kaolin.models.PointNet import PointNetClassifier
 import kaolin.transforms as tfs
 from utils import visualize_batch
