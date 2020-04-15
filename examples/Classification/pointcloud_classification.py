22b2bc6e346b71db4c9158a668272d9a0315036d
akashgokul <akashgokul@berkeley.edu>
2020-04-15 00:19:33 -0700

    trying to fix imports

--- examples/Classification/pointcloud_classification.py
+++ examples/Classification/pointcloud_classification.py
@@ -4,8 +4,6 @@ from tqdm import tqdm
 import torch
 from torch.utils.data import DataLoader
 
-import sys
-sys.path.append("..")
 
 from kaolin.datasets import ModelNet, Scan2CAD
 from kaolin.models.PointNet import PointNetClassifier
