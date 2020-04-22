838e7b47edfac396b563c1cca01f5a5b2b17fbed
akashgokul <akashgokul@berkeley.edu>
2020-04-22 07:39:47 -0700

    updating examples copies

--- examples/Classification/pointcloud_classification_scan2cad.py
+++ examples/Classification/pointcloud_classification_scan2cad.py
@@ -8,7 +8,7 @@ import sys
 sys.path.append("..")
 from kaolin.datasets import ModelNet, Scan2CAD
 from kaolin.models.PointNet import PointNetClassifier
-#import kaolin.transforms as tfs
+import kaolin.transforms as tfs
 from utils import visualize_batch
 import pandas as pd
 
