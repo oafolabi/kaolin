294a0c0d73e364d21374f8eaa695d6bbdafa0a6d
akashgokul <akashgokul@berkeley.edu>
2020-04-25 15:41:42 -0700

    Performing split

--- kaolin/datasets/scan2cad.py
+++ kaolin/datasets/scan2cad.py
@@ -51,7 +51,7 @@ class Scan2CAD(object):
         assert split in ['train' ,'test']
         data_frame.rename(columns={'Unnamed: 0': 'Filepath', '0':'ID'})
         self.data_frame = data_frame
-        print(Self.data_frame.columns)
+        print(self.data_frame.columns)
         self.transform = transform
         self.device = device
         self.names = []
