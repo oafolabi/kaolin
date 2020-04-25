35719a17a0232898459eaf43c02435de82a29b33
akashgokul <akashgokul@berkeley.edu>
2020-04-25 14:23:19 -0700

    Performing split

--- kaolin/datasets/scan2cad.py
+++ kaolin/datasets/scan2cad.py
@@ -61,6 +61,7 @@ class Scan2CAD(object):
         
         if(split == 'train' or split == 'validation'):
             single_ct_cad_ids = self.cad_ids.value_counts()
+            print("Single ct")
             print(single_ct_cad_ids)
             print(single_ct_cad_ids.head())
             assert 3==2
