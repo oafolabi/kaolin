84383eb4a5d0e74c511b635fd9ac9ef9d070cd3c
akashgokul <akashgokul@berkeley.edu>
2020-04-25 18:03:24 -0700

    Performing split

--- kaolin/datasets/scan2cad.py
+++ kaolin/datasets/scan2cad.py
@@ -90,9 +90,7 @@ class Scan2CAD(object):
         train_sample_indices = shuffled_indices[0:num_train_samples]
 
         train_indices = train_sample_indices + single_indices
-        print(len(train_indices))
-        print(len(single_indices) / len(data_frame))
-        print(0.6*len(data_frame))
+        print(0.6*len(data_frame) - len(train_indices))
         val_indices = shuffled_indices[num_train_samples : num_train_samples + num_val_samples]
         test_indices = shuffled_indices[len(rest_of_data_frame)-num_test_samples:]
         print(len(data_frame) - (len(train_indices + val_indices + test_indices)))
