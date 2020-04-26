65ee481ebc3116ac7acc149070c9b8135debe01f
akashgokul <akashgokul@berkeley.edu>
2020-04-25 18:07:32 -0700

    Performing split

--- kaolin/datasets/scan2cad.py
+++ kaolin/datasets/scan2cad.py
@@ -92,9 +92,8 @@ class Scan2CAD(object):
         train_indices = train_sample_indices + single_indices
         val_indices = shuffled_indices[num_train_samples : num_train_samples + num_val_samples]
         test_indices = shuffled_indices[len(rest_of_data_frame)-num_test_samples:]
-        print(len(train_indices))
-        print(len(val_indices))
-        print(len(test_indices))
+        print(len(train_indices) + len(val_indices) + len(test_indices) == len(data_frame))
+        print(set(train_indices + val_indices + test_indices) == set(list(range(0,len(data_frame)))))
 
         #creates train and validation set
         self.train_data_frame = data_frame.iloc[train_indices]
