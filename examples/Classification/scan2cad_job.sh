b3f2ff2682888af122b1997196bdd62213a39b0f
akashgokul <akashgokul@berkeley.edu>
2020-04-25 21:51:47 -0700

    Performing split

--- examples/Classification/scan2cad_job.sh
+++ examples/Classification/scan2cad_job.sh
@@ -15,12 +15,11 @@
 #SBATCH --ntasks-per-node=24
 #
 # Wall clock limit:
-#SBATCH --time=15:00:00
+#SBATCH --time=20:00:00
 #
 ## Command(s) to run (example):
 module load python
 module load cuda/10.1
-cd /global/scratch/akashgokul
 source activate /global/scratch/akashgokul/kaolin_run
 cd kaolin/examples/Classification
-python3 pointcloud_classification_scan2cad.py --run_number 1 --tensorboard False --batch_size 256 --epochs 20
+python3 pointcloud_classification_scan2cad.py --run_number 1 --tensorboard False --batch_size 64 --epochs 10
