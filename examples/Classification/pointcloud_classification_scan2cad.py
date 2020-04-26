0e4c8a59fece3534674e39ff1506e434d6dac5a3
akashgokul <akashgokul@berkeley.edu>
2020-04-26 00:10:34 -0700

    Performing split

--- examples/Classification/pointcloud_classification_scan2cad.py
+++ examples/Classification/pointcloud_classification_scan2cad.py
@@ -42,10 +42,10 @@ train_dataset = Scan2CAD(data_frame,split='train',transform=transform, device=ar
 train_loader = DataLoader(train_dataset,batch_size=args.batch_size, shuffle=True)
 
 val_dataset = Scan2CAD(data_frame,split='validation',transform=transform, device=args.device)
-val_loader = DataLoader(val_dataset,batch_size=args.batch_size // 2, shuffle=True)
+val_loader = DataLoader(val_dataset,batch_size=12, shuffle=True)
 
 test_dataset = Scan2CAD(data_frame,split='test',transform=transform, device=args.device)
-test_loader = DataLoader(test_dataset,batch_size=args.batch_size // 2, shuffle=True)
+test_loader = DataLoader(test_dataset,batch_size=12, shuffle=True)
 
 #Same num_classes for all datasets
 num_cad_classes = train_dataset.get_num_classes()
@@ -134,7 +134,7 @@ for e in range(args.epochs):
 
 model.eval()
 
-test_acc = 0
+test_acc = 0.
 with torch.no_grad():
     num_batches = 0
     for idx, test_batch in enumerate(tqdm(test_loader)):
