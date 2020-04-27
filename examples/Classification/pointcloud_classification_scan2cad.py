99a686082e8b46feffa172888c3754b945609bc1
akashgokul <akashgokul@berkeley.edu>
2020-04-27 12:07:12 -0700

    Updating

--- examples/Classification/pointcloud_classification_scan2cad.py
+++ examples/Classification/pointcloud_classification_scan2cad.py
@@ -17,7 +17,7 @@ import pandas as pd
 
 parser = argparse.ArgumentParser()
 parser.add_argument('--run_number', type = str, default = '0', help='Run Number of log')
-parser.add_argument('--tensorboard', type = int, default = 0, choice=[0,1],help='T/F To use Tensorboard or not')
+parser.add_argument('--tensorboard', type = int, default = 0, choices=[0,1],help='T/F To use Tensorboard or not')
 #parser.add_argument('--modelnet-root', type=str, help='Root directory of the ModelNet dataset.')
 #parser.add_argument('--categories', type=str, nargs='+', default=['chair', 'sofa'], help='list of object classes to use.')
 parser.add_argument('--num-points', type=int, default=1024, help='Number of points to sample from meshes.')
