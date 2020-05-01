a4f96edf9a8054384e2026ac910bc2d161957f75
akashgokul <akashgokul@berkeley.edu>
2020-04-30 20:08:22 -0700

    fixing

--- data_preproc.py
+++ data_preproc.py
@@ -3,16 +3,16 @@ import pandas as pd
 import open3d as o3d
 import numpy as np
 
-#TRYING Transfer Learning
-ROOTDIR = "/global/scratch/akashgokul/mined_scannet_chairs/"
+ROOTDIR = "/global/scratch/oafolabi/data/mined_scannet_chairs/"
 
 print("------"*10)
 print("This code generates a csv file containing the directory of each off file and it's CAD id")
 print("------"*10)
 
 def transform_pcd(root_dir, ply_dir):
+    print(ply_dir)
     mesh = o3d.io.read_triangle_mesh(ply_dir)
-    off_dir = root_dir + "/model_normalized.obj"
+    off_dir = root_dir + "/mesh_model.obj"
     o3d.io.write_triangle_mesh(off_dir, mesh)
     return off_dir
 
@@ -22,17 +22,14 @@ number_of_chair_cad_models = 236778 #from shapenet
 scenes = os.listdir(ROOTDIR)
 for scene in scenes:
     i += 1
-    if(scene == 'data.csv'):
-        continue
     print("Processing Scene: " + str(i) + " / " + str(len(scenes)))
     for chair_dir in os.listdir(ROOTDIR + "/" + scene):
         chair_dir_path = ROOTDIR + scene + "/" + chair_dir
         chair_dir_contents = os.listdir(chair_dir_path)
         cad_id_file = open(chair_dir_path + "/id_cad.txt", "r")
         cad_id = cad_id_file.readline()
-        #TRYING Transfer Learning
-        ply_dir = chair_dir_path + "/model_normalized.ply"
-        print(ply_dir)
+
+        ply_dir = chair_dir_path + "/cropped_mesh.ply"
         if(os.path.exists(ply_dir)):
             off_dir = transform_pcd(chair_dir_path, ply_dir)
             chair_id_dict[off_dir] = cad_id
